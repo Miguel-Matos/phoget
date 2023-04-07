6d47b38b3b95839ec5d568f3a0e1a98672388a42
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-04-07 18:53:50 +0900

    Menu rough draft completed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { navbar } from './content';
 import { main } from './content';
 import { foot } from './content';
 import { home } from './home';
+import { menu } from './menu';
 
 document.body.appendChild(navbar());
-document.body.appendChild(main(home.title, home.about));
+document.body.appendChild(main(menu.title, menu.about));
 document.body.appendChild(foot());
---
+++ src/menu.js
@@ -0,0 +1,38 @@
+const item = (name, price, detail, src) => {
+  const foodName = document.createElement('h3');
+  foodName.textContent = name;
+  const cost = document.createElement('p');
+  cost.textContent = "￥" + price;
+  const description = document.createElement('p');
+  description.textContent = detail;
+  const image = document.createElement('img');
+  image.src = src;
+  image.classList.add('img');
+  return {foodName, cost, description, image};
+};
+
+const menu = (() => {
+  const title = "Menu";
+  const about = document.createElement('div');
+
+  function adding (name, price, detail, src) {
+    const arr = [name, price, detail, src];
+    for (let i = 0; i < 4; i++) {
+      about.appendChild(arr[i]);
+    }
+  };
+
+  const bahnmi = item("Bahn Mi", "699", "Vietnamese people consume 23 daily. Fresh roasted bread with garden grown vegetables and meat of your choosing.", "../src/img/bahnmi.jpg");
+  adding(bahnmi.foodName, bahnmi.cost, bahnmi.description, bahnmi.image);
+
+  const pho = item("Pho", "799", "The signature dish of our restaurant. No other explanation needed.", "../src/img/pho.jpg");
+  adding(pho.foodName, pho.cost, pho.description, pho.image);
+
+  const rolls = item("Spring Rolls", "399", "A special blend of seafood and vegetables, guaranteed to keep you coming back for more!", "../src/img/springroll.jpg");
+  adding(rolls.foodName, rolls.cost, rolls.description, rolls.image);
+
+  return {title, about};
+})();
+
+
+export {menu}; 
