81903efbd9ace11fca124c4eb0290c8b5779e1ce
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-04-08 22:05:38 +0900

    TODO create module for site that swaps out content

--- src/index.js
+++ src/index.js
@@ -6,22 +6,17 @@ import { home } from './home';
 import { menu } from './menu';
 
 const core = document.createElement('div');
-
 const navArea = navbar.nav;
 const homepage = main(home.title, home.about);
 const menupage = main(menu.title, menu.about);
 const footer = foot();
 
+navbar.menu.addEventListener('click', () => {
+  core.removeChild(homepage);
+  core.appendChild(menupage);
+});
+
 document.body.appendChild(navArea);
 document.body.appendChild(core);
 core.appendChild(homepage);
-
-
-  navbar.menu.addEventListener('click', () => {
-    core.removeChild(homepage);
-    core.appendChild(menupage);
-  });
-
-
-
 document.body.appendChild(footer);
