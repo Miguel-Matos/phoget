5b86692b85e1866dd4da8d2bf70d12d6cabee883
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-04-08 11:54:40 +0900

    Updated switching logic

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import { foot } from './content';
 import { home } from './home';
 import { menu } from './menu';
 
-document.body.appendChild(navbar.nav);
 
+const navbar = navbar.nav;
 const homepage = main(home.title, home.about);
 const menupage = main(menu.title, menu.about);
 const footer = foot();
+
+document.body.appendChild(navbar);
 document.body.appendChild(homepage);
 
 
   navbar.menu.addEventListener('click', () => {
     document.body.removeChild(homepage);
-    document.body.removeChild(footer);
-    document.body.appendChild(menupage);
-    document.body.appendChild(footer);
+    menupage.insertAdjacentElement('afterbegin', navbar);
   });
 
 
